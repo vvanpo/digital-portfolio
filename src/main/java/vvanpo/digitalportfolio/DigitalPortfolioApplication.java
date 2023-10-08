fc9102e4fb6332248b54561e915d3fd90e1112f0
Victor van Poppelen <vvanpo@gmail.com>
2023-10-08 09:14:38 -0400

    Init
    Created via:
    https://start.spring.io/#!type=gradle-project&language=java&platformVersion=3.1.4&packaging=jar&jvmVersion=17&groupId=vvanpo&artifactId=digital-portfolio&name=digital-portfolio&description=Learning%20project&packageName=vvanpo.digital-portfolio&dependencies=devtools,lombok,configuration-processor,docker-compose,web,security,data-jpa,flyway,h2,postgresql,validation

---
+++ src/main/java/vvanpo/digitalportfolio/DigitalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package vvanpo.digitalportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPortfolioApplication.class, args);
+	}
+
+}
